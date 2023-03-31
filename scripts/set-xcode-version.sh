75454eb3843a460f6e0c9c3a08565c55603a51cd
Matthew Bell <mbell.dvsa@icloud.com>
2023-03-31 12:34:21 +0100

    a

--- scripts/set-xcode-version.sh
+++ scripts/set-xcode-version.sh
@@ -8,7 +8,7 @@ echo "Setting version to: ${VERSION}"
 # Pass VERSION as an argument to update config.xml
 node scripts/set-config-version.js ${VERSION}
 
-# Update plist files
-cd ios/App
-xcrun agvtool new-marketing-version ${VERSION}
-xcrun agvtool new-version -all ${VERSION}
+## Update plist files
+#cd ios/App
+#xcrun agvtool new-marketing-version ${VERSION}
+#xcrun agvtool new-version -all ${VERSION}
