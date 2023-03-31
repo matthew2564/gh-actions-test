5e52ee6b2c814c79b5adaafa5c4eb547c33708f1
Matthew Bell <33056264+matthew2564@users.noreply.github.com>
2023-03-31 10:54:03 +0100

    Test 8 (#11)
    * test-7
    
    * test-8
    
    * 0.0.2
    
    * test-8
    
    * 0.1.0
    
    * A
    
    * 0.1.1
    
    * a

---
+++ scripts/set-config-version.js
@@ -0,0 +1,40 @@
+const fs = require('fs');
+const xml2js = require('xml2js');
+const semver = require('semver');
+
+const [newVersion] = process.argv.slice(2);
+if (!newVersion) {
+  throw new Error('newVersion is undefined');
+}
+
+if (!semver.valid(newVersion)) {
+  throw new Error(`Semver version invalid: ${newVersion}`);
+}
+
+const configFile = 'config.xml';
+
+// Read config.xml
+fs.readFile(configFile, 'utf8', (err, xml) => {
+  if (err) {
+    return console.error(err);
+  }
+
+  // Parse XML to JS Obj
+  xml2js.parseString(xml, (err, obj) => {
+    if (err) {
+      return console.error(err);
+    }
+
+    obj.widget['$'].version = newVersion;
+    const builder = new xml2js.Builder();
+    const xml = builder.buildObject(obj);
+
+    fs.writeFile(configFile, xml, (err) => {
+      if (err) {
+        return console.error(err);
+      }
+
+      console.log(`Build number successfully incremented to ${newVersion}`);
+    });
+  });
+});
